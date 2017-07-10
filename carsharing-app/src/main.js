679af24db510df14c6dce0b8164bdb9cf9dc382b
liangming.zhu <liangming.zhu@foxmail.com>
2017-07-10 09:40:24 +0800

    Update main.js

--- carsharing-app/src/main.js
+++ carsharing-app/src/main.js
@@ -4,12 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'vue-awesome/icons'
-import '../library/mint-ui'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import store from '@/store/index'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import AMap from 'vue-amap';
 
+Vue.use(MintUI);
 Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
