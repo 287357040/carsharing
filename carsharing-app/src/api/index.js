f638c831a86d05c1ad9b3a58ff667d61d56d0244
Jim Hu <287357040@qq.com>
2017-08-02 11:31:35 +0800

    去除response判断 统一在拦截方法实现

--- carsharing-app/src/api/index.js
+++ carsharing-app/src/api/index.js
@@ -6,12 +6,6 @@ var HttpResHelper = {
         if (response.data && response.data.res == 1 && successFunc)
             successFunc(response.data.obj);
         else {
- 
-            if (response.data.res == 301) {
-                // 跳转到登陆页面
-                store.dispatch("set_login_state", 0);
-                store.push({ path: '/login' });
-            }
             if (errorFunc) {
                 errorFunc({
                     "code": response.data.res,
