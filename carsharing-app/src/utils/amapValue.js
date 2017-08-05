70c920bb3730e49a6db72f9a2d5b34e39876f5fd
liangwei0101 <liangwei0101@users.noreply.github.com>
2017-08-05 13:41:21 +0800

    更改相关接口，将地图的接口改为起始和目的

--- carsharing-app/src/utils/amapValue.js
+++ carsharing-app/src/utils/amapValue.js
@@ -1,18 +1,34 @@
+// ��������,��Ϊ��ͼ�������ݵĴ�Ŵ�
 export const mapData = {
-  amapInfo:{}
+  // amapInfo:{},
+  startAmapInfo:{},   // 起始地址信息
+  endAmapInfo:{}      // 终点地址信息
+  // LocationFlag: -1 //是否定位标志
 }
-export const setMapInfo = (value) => mapData.amapInfo = value
-export const getMapInfo = () => { return mapData.amapInfo }
+// export const setMapInfo = (value) => mapData.amapInfo = value
+// export const getMapInfo = () => { return mapData.amapInfo }
+export const setStartMapInfo = (value) => mapData.startAmapInfo = value
+export const getStartMapInfo = () => { return mapData.startAmapInfo }
+export const setEndMapInfo = (value) => mapData.endAmapInfo = value
+export const getEndMapInfo = () => { return mapData.endAmapInfo }
+// export const incLocationFlag = () => mapData.LocationFlag++
+// export const getLocationFlag = () => { return mapData.LocationFlag }
 
 /**
- * @@exports.default {Mixin}
+ * @exports.default {Mixin}
  */
 export default {
   data: () => ({
     mapData
   }),
   methods: {
-    setMapInfo,
-    getMapInfo
+    // setMapInfo,
+    // getMapInfo,
+       setStartMapInfo,
+       getStartMapInfo,
+       setEndMapInfo,
+       getEndMapInfo
+    // incLocationFlag,
+    // getLocationFlag
   }
 }
