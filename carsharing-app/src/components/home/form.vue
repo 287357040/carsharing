755fe57f0e4bfbb57028c1aa61d401cc1908b3d7
liangwei0101 <liangwei0101@users.noreply.github.com>
2017-08-05 13:40:30 +0800

    修改对进去是定位的，不能手选起始地址的情况

--- carsharing-app/src/components/home/form.vue
+++ carsharing-app/src/components/home/form.vue
@@ -1,81 +1,84 @@
 <template>
   <div>
-  <form class="form-wrapper clearfix" onsubmit="return false">
-    <div class="home-form clearfix">
-      <div class="company-home-location clearfix">
-        <div class="location-form border-bottom-1px" @click="show_suggest('getOn')">
-          <div class="location-field">
-            <i class="icon-corporation" style="color: #FE872B"></i>&nbsp;&nbsp;
-            <span class="location-title ">起始地</span>
+    <form class="form-wrapper clearfix" onsubmit="return false">
+      <div class="home-form clearfix">
+        <div class="company-home-location clearfix">
+          <div class="location-form border-bottom-1px" @click="show_suggest('getOn')">
+            <div class="location-field">
+              <i class="icon-corporation" style="color: #FE872B"></i>&nbsp;&nbsp;
+              <span class="location-title ">公司</span>
               <span class="location-address">{{startPlaceShow}}</span>
+            </div>
           </div>
-        </div>
-        <div class="location-form" @click="show_suggest('getOff')">
-          <div class="location-field">
-            <i class="icon-home"></i>&nbsp;&nbsp;
-            <span class="location-title ">目的地</span>
+          <div class="location-form" @click="show_suggest('getOff')">
+            <div class="location-field">
+              <i class="icon-home"></i>&nbsp;&nbsp;
+              <span class="location-title ">回家</span>
               <span class="location-address">{{endPlace.name}}</span>
+            </div>
+          </div>
+        </div>
+        <div class="setting-info clearfix">
+          <div class="location-field order-time" @click="openDatetimePicker">
+            <i class="icon-date"></i>
+            <span class="info-text">{{departureTime || '出行时间'}}</span>
+            <!--预约时间弹窗-->
+            <mt-popup v-model="datetimePopup" position="bottom" class="mint-popup">
+              <div class="picker-toolbar clearfix">
+                <span class="mint-datetime-cancel" @click.stop="datetimeCancle">取消</span>
+                <span class="mint-datetime-title">出行时间</span>
+                <span class="mint-datetime-confirm" @click.stop="datetimeConfirm">确定</span>
+              </div>
+              <mt-picker :slots="datetimeSlots" @change="onDatetimeChange" :visible-item-count="4"></mt-picker>
+            </mt-popup>
+          </div>
+          <div class="location-field order-population" @click="openSeatsPicker">
+            <span class="icon-ren" style="padding-left:10px">
+              <span class="path1"></span>
+              <span class="path2"></span>
+            </span>
+            <span class="info-text">{{seatsCounts || seatsDescription}}</span>
+  
+            <mt-popup v-model="popupVisible" position="bottom" class="mint-popup">
+              <div class="picker-toolbar clearfix">
+                <span class="mint-datetime-cancel" @click.stop="cancleSeats">取消</span>
+                <span class="mint-datetime-title">乘车人数</span>
+              </div>
+              <ul class="seats-choice-area">
+                <li :key="item" v-for="item in seatSlots" class="seat-li" @click.stop="choiceSeats(item)">{{item}}</li>
+              </ul>
+            </mt-popup>
+          </div>
+        </div>
+        <div class="setting-info clearfix">
+          <div class="location-field  remark">
+            <i class="icon-Stroke01"></i>
+            <input type="text" class="remark-info info-text" placeholder="出行需求">
+            </input>
           </div>
         </div>
       </div>
-      <div class="setting-info clearfix">
-        <div class="location-field order-time" @click="openDatetimePicker">
-          <i class="icon-date"></i>
-          <span class="info-text">{{departureTime || '出行时间'}}</span>
-          <!--预约时间弹窗-->
-          <mt-popup v-model="datetimePopup" position="bottom" class="mint-popup">
-            <div class="picker-toolbar clearfix">
-              <span class="mint-datetime-cancel" @click.stop="datetimeCancle">取消</span>
-              <span class="mint-datetime-title">出行时间</span>
-              <span class="mint-datetime-confirm" @click.stop="datetimeConfirm">确定</span>
-            </div>
-            <mt-picker :slots="datetimeSlots" @change="onDatetimeChange" :visible-item-count="4"></mt-picker>
-          </mt-popup>
-        </div>
-        <div class="location-field order-population" @click="openSeatsPicker">
-          <span class="icon-ren" style="padding-left:10px">
-            <span class="path1"></span>
-            <span class="path2"></span>
-          </span>
-          <span class="info-text">{{seatsCounts || seatsDescription}}</span>
-  
-          <mt-popup v-model="popupVisible" position="bottom" class="mint-popup">
-            <div class="picker-toolbar clearfix">
-              <span class="mint-datetime-cancel" @click.stop="cancleSeats">取消</span>
-              <span class="mint-datetime-title">乘车人数</span>
-            </div>
-            <ul class="seats-choice-area">
-              <li :key="item" v-for="item in seatSlots" class="seat-li" @click.stop="choiceSeats(item)">{{item}}</li>
-            </ul>
-          </mt-popup>
-        </div>
-      </div>
-      <div class="setting-info clearfix">
-        <div class="location-field  remark">
-          <i class="icon-Stroke01"></i>
-          <input type="text" class="remark-info info-text" placeholder="出行需求">
-          </input>
-        </div>
-      </div>
-    </div>
-    <p class="expain">约
-      <span>21.3km</span>
-    </p>
-    <div class="search-issue-wrapper">
+      <p class="expain">约
+        <span>21.3km</span>
+      </p>
+      <div class="search-issue-wrapper">
         <mt-button type="default" class="comfirm-search-btn" @click="getInfo">查询</mt-button>
-      <mt-button type="default" class="confirm-issue-btn linear-gradient-bg" @click="issueRoute">发布</mt-button>
-    </div>
-  </form>
+        <mt-button type="default" class="confirm-issue-btn linear-gradient-bg" @click="issueRoute">发布</mt-button>
+      </div>
+    </form>
     <!--定位地图需要的地图插件（不显示）-->
     <el-amap :plugin="plugin"></el-amap>
     <!--定位地图需要的地图插件（不显示）-->
+
     <!-- <code>{{ getMapInfo() }}</code> -->
+
   </div>
 </template>
+
 <script>
-import bus from '@/utils/eventBus'
+import bus from '@/scripts/eventBus'
 import apiHandler from '@/api/services/demand.service'
-import sharedStateMixin from '@/utils/amapValue'
+import sharedStateMixin from '@/scripts/amapValue'
 
 export default {
   mixins: [sharedStateMixin],
@@ -86,9 +89,6 @@ export default {
       seatsCounts: '',
       seatsDescription: '同行的几人？',
       dateTime: '',
-      isMoveDate: false,
-      isMoveHour: false,
-      isCarry: false,
       popupVisible: false,
       datetimePopup: false,
       seatSlots: ['1人', '2人', '3人', '4人'],
@@ -129,19 +129,23 @@ export default {
       startPlace: {},
       endPlace: {
         name: ''
-      }
+      },
+      isLocationFlag:''
     }
   },
   created: function () {
-    this.computedDate();
-    this.computedMinutes();
-    this.computedHours();
-    this.endPlace = this.getMapInfo();
+    this.computedDatetime();
+    this.isLocation()  //是否采用定位还是手动
+    this.endPlace = this.getEndMapInfo()
   },
   methods: {
     show_suggest(key) {
       this.$store.dispatch('show_suggest', key)
-      this.$router.push({ path: '/mapLocation' })
+      if(key === 'getOn'){
+        this.$router.push({ path: '/mapLocation', query: {params: 'getOn'} })
+      }else{
+        this.$router.push({ path: '/mapLocation', query: {params: 'getOff'} })
+      }
     },
     openDatetimePicker() {
       this.datetimePopup = true;
@@ -167,26 +171,12 @@ export default {
       this.data.seatsCounts = values[0];
     },
     onDatetimeChange: function (picker, values) {
-      let [currentDate, currentHour, currentMinute] = [values[0], values[1], values[2]];
-
       if (!values.includes(undefined)) {
-        if (!isNaN(values[1].substring(1, 2))) {
-          this.dateTime = currentDate + ' ' + currentHour.substring(0, 2) + ':' + currentMinute.substring(0, 2);
-        } else {
-          this.dateTime = currentDate + ' ' + currentHour.substring(0, 1) + ':' + currentMinute.substring(0, 2);
-        }
-        if (currentDate != '今天')
-          this.isMoveDate = true;
-        else this.isMoveDate = false;
-        if (currentHour.substring(0, currentHour.length - 1) != (new Date).getHours())
-          this.isMoveHour = true;
-        else this.isMoveHour = false;
-      } else {
-        this.dateTime = currentDate + ' ' + (new Date).getHours() + ':' +
-          (new Date).getMinutes();
+        this.dateTime = values[0] + ' ' + values[1].substring(0, 2) + ':' + values[2].substring(0, 2);
       }
+
     },
-    computedDate: function () {
+    computedDatetime: function () {
       var date = new Date();
       //处理日期
       for (let i = 0; i < 10; i++) {
@@ -200,20 +190,22 @@ export default {
         }
         this.datetimeSlots[0].values.push(someDates);
       }
-    },
-    computedMinutes() {
+      //处理小时
+      for (let i = 0; i < (24 - date.getHours()); i++) {
+        let someHours = (date.getHours() + i) + '点';
+        this.datetimeSlots[1].values.push(someHours);
+      }
+
       //处理分钟
-      var date = new Date(),
-        newArry = [],
-        newStr = null,
-        currentMinute = date.getMinutes(),
-        minutesArry = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
+      var currentMinute = date.getMinutes();
+      var newArry = [];
+      var newStr = '';
+      var minutesArry = new Array(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55);
       for (let i = 0; i < minutesArry.length; i++) {
-        if (currentMinute >= minutesArry[i] && currentMinute <= minutesArry[i + 1]) {
+        if (currentMinute > minutesArry[i] && currentMinute < minutesArry[i + 1]) {
           newArry = minutesArry.slice(i + 2);
         } else if (currentMinute > 50) {
           newArry = minutesArry;
-          this.isCarry = true;
         }
       }
       for (let i = 0; i < newArry.length; i++) {
@@ -226,23 +218,10 @@ export default {
       }
     },
     issueRoute: function () {
-      this.publishInfo();
+      this.publishInfo()
       this.$router.push({ path: '/awaitStatus' });
     },
-    computedHours() {
-      //处理小时
-      var date = new Date();
-      var someHours = null;
-      for (let i = 0; i < (24 - date.getHours()); i++) {
-        if (this.isCarry) {
-          someHours = (date.getHours() + i + 1) + '点';
-        } else {
-          someHours = (date.getHours() + i) + '点';
-        }
-        this.datetimeSlots[1].values.push(someHours);
-      }
-    },
-	// 地址过长截取函数 如：浙江省杭州市滨江区浦沿街道火炬大道恒生大厦(园支一路)--》火炬大道恒生大厦(园支一路)
+    // 地址过长截取函数 如：浙江省杭州市滨江区浦沿街道火炬大道恒生大厦(园支一路)--》火炬大道恒生大厦(园支一路)
     placeSlice(locationInfo) {
       if (locationInfo) {
         this.startPlace = locationInfo
@@ -266,8 +245,6 @@ export default {
     },
     // 点击发布
     publishInfo() {
-      // console.log(this.startPlace)
-      // console.log(this.endPlace)
       let data = {
         startArea: this.startPlace.addressComponent.district, // 起始区县
         startPlace: this.startPlace.formattedAddress, // 起始地址
@@ -290,42 +267,35 @@ export default {
       }, (err)=>{
         console.log('我是发布错误')
       })
+    },
+    //是否采用定位或者手动地址
+    isLocation() {
+      this.isLocationFlag = this.$route.query.params
+      if(this.$route.query.params === undefined){ //起始地址采用定位
+          console.log('1111111111')
+      }
+      if(this.$route.query.params ==='LocationFlag'){ //目的地址采用地图选中
+        this.startPlace = this.getStartMapInfo()
+          console.log('222222')
+      }
     }
 
   },
+  // 定位消息获得以后
   watch: {
-    isMoveDate: function () {
-      var allHours = null;
-      this.datetimeSlots[1].values = [];
-      if (this.isMoveDate) {
-        for (let i = 0; i < 24; i++) {
-          allHours = i + '点';
-          this.datetimeSlots[1].values.push(allHours);
-        }
-      } else {
-        this.computedHours();
+    locationInfo: function () {
+      if(this.isLocationFlag === 'LocationFlag'){
+        console.log('我是不定位')
+        this.startPlace = this.getStartMapInfo()
+        console.log(this.startPlace)
+        this.startPlaceShow = this.startPlace.name
+        return
+      } else{
+        console.log('我是定位')
+        this.startPlace = this.locationInfo
+        this.startPlaceShow = this.placeSlice(this.locationInfo)[1]
+         // this.startPlace = this.locationInfo.formattedAddress
       }
-    },
-    isMoveHour: function () {
-      var allMinutes = null;
-      var allMinutesArry = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
-      this.datetimeSlots[2].values = [];
-      if (this.isMoveHour || this.isMoveDate) {
-        for (let i = 0; i < allMinutesArry.length; i++) {
-          allMinutes = allMinutesArry[i] + '分';
-          this.datetimeSlots[2].values.push(allMinutes);
-        }
-      } else {
-        this.computedMinutes();
-      }
-    },
-	locationInfo: function () {
-      this.startPlace = this.locationInfo
-      this.startPlaceShow = this.placeSlice(this.locationInfo)[1]
-      // this.startPlace = this.locationInfo.formattedAddress
-    },
-    mapData: function () {
-      alert('1121121212')
     }
   }
 }
