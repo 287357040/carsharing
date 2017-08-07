3787adf441b51070d11dc43b353ffebc185430e8
liangwei0101 <liangwei0101@users.noreply.github.com>
2017-08-07 16:29:14 +0800

    增加对从数据库查出地址的对象的统一转换

--- carsharing-app/src/utils/amapValue.js
+++ carsharing-app/src/utils/amapValue.js
@@ -1,16 +1,45 @@
 // ��������,��Ϊ��ͼ�������ݵĴ�Ŵ�
 export const mapData = {
   // amapInfo:{},
-  startAmapInfo: {},   // 起始地址信息
-  endAmapInfo: {}      // 终点地址信息
-  // LocationFlag: -1 //是否定位标志
+  startAmapInfo: {},    // 起始地址信息
+  endAmapInfo: {},      // 终点地址信息
+  transAmapInfo:{      // 统一返回对象 
+    district: '', // 区县
+    name: '', // 地址
+    userNo: '',
+    location: {
+      lat:'',
+      lng: ''
+    }
+  }    
 }
 // export const setMapInfo = (value) => mapData.amapInfo = value
 // export const getMapInfo = () => { return mapData.amapInfo }
 export const setStartMapInfo = (value) => mapData.startAmapInfo = value
-export const getStartMapInfo = () => { return mapData.startAmapInfo }
+export const getStartMapInfo = () => { 
+  if(mapData.startAmapInfo.userNo === undefined){  //是存的选中后的地址
+    return mapData.startAmapInfo
+  } else{ // 从数据库查出的对象
+      mapData.transAmapInfo.district = mapData.startAmapInfo.address
+      mapData.transAmapInfo.name = mapData.startAmapInfo.name
+      mapData.transAmapInfo.location.lat = mapData.startAmapInfo.latitude
+      mapData.transAmapInfo.location.lng = mapData.startAmapInfo.longitude
+      mapData.transAmapInfo.userNo = mapData.startAmapInfo.userNo
+    return mapData.transAmapInfo
+  }
+}
 export const setEndMapInfo = (value) => mapData.endAmapInfo = value
-export const getEndMapInfo = () => { return mapData.endAmapInfo }
+export const getEndMapInfo = () => { 
+  if(mapData.endAmapInfo.userNo === undefined){ //是存的选中后的地址
+    return mapData.endAmapInfo
+  } else{  // 从数据库查出的对象
+      mapData.transAmapInfo.district = mapData.endAmapInfo.address
+      mapData.transAmapInfo.name = mapData.endAmapInfo.address
+      mapData.transAmapInfo.location.lat = mapData.endAmapInfo.latitude
+      mapData.transAmapInfo.location.lng = mapData.endAmapInfo.longitude
+      return mapData.transAmapInfo
+  }
+}
 // export const incLocationFlag = () => mapData.LocationFlag++
 // export const getLocationFlag = () => { return mapData.LocationFlag }
 
