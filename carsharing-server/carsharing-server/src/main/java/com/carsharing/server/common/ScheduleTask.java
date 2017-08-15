fec4b2f53ea9dd4e2488e62b75b934e5df55da3c
Jim Hu <287357040@qq.com>
2017-08-15 15:17:04 +0800

    修复定时任务问题

--- carsharing-server/carsharing-server/src/main/java/com/carsharing/server/common/ScheduleTask.java
+++ carsharing-server/carsharing-server/src/main/java/com/carsharing/server/common/ScheduleTask.java
@@ -6,23 +6,27 @@ import com.carsharing.server.mapper.RideRouteMapper;
 import com.carsharing.server.service.RideRouteService;
 import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.Date;
 
-
+@Component
 public class ScheduleTask {
 
+    public final static long ONE_Minute =  60 * 1000;
     private Logger lo = Logger.getLogger(ScheduleTask.class);
+
     @Resource
     private RideDemandMapper rideDemandMapper;
 
     @Resource
     private RideRouteMapper rideRouteMapper;
 
-    @Scheduled(cron = "01 00 00 * * ?")
-    public void clearOverdueData(Date date) {
+    @Scheduled(cron = "1 0 0 * * ?")
+    public void clearOverdueData() {
         try {
+            Date date = new Date();
             rideDemandMapper.deleteOverdueDemands(date);
             rideRouteMapper.deleteOverdueRoutes(date);
         }catch (Exception e){
